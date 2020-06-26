ba52c04bd12fa6fef571bd04317b7173077312c5
A1doranM <vaselisk1997@gmail.com>
2020-06-26 15:37:36 +0300

    added dummy data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personalexpensesapp/transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,6 +16,22 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.69, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New Balls', amount: 100.69, date: DateTime.now()),
+    Transaction(
+        id: 't3', title: 'New t-shirts', amount: 29.69, date: DateTime.now()),
+    Transaction(
+        id: 't4',
+        title: 'Weekly Groceries',
+        amount: 39.69,
+        date: DateTime.now()),
+    Transaction(
+        id: 't5', title: 'Ethernet', amount: 9.69, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
